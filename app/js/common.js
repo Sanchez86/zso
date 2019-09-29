933cd5f98d311da8f68a642a761b737ca305b183
sanchez <0719118@gmail.com>
2019-09-29 18:54:17 +0200

    add slider

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,23 @@
-$(function() {
-
-	// Custom JS
-
+// Custom JS
+//console.log(1);
+$(document).ready(function () {
+    (function ($) {
+        $('.owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            autoplay:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        });
+    })(jQuery);
 });
